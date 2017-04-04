10a1f87473eba17a9cabc856b2597fa3cd6f4159
centeio <c.centeiojorge@outlook.pt>
2017-04-04 23:31:39 +0100

    print children

--- src/main/PAT.java
+++ src/main/PAT.java
@@ -1,5 +1,8 @@
 package main;
 
+import pack.MyNewGrammar;
+import pack.Node;
+import pack.SimpleNode;
 import parser.Parser;
 
 public class PAT {
@@ -8,5 +11,24 @@ public class PAT {
 		Parser parser = new Parser();
 		
 		parser.parse();
+		MyNewGrammar.createjjt("if(x==null){x=3}");
+		
+		SimpleNode n = MyNewGrammar.n;
+		printChildren(n);
+		
+	}
+	
+	public static void printChildren(SimpleNode node){
+		if(node.jjtGetNumChildren()==0){
+			System.out.println(node.type + " " + node.jjtGetValue());
+			return;
+		}
+		
+		int i = 0;
+
+		while(i<node.jjtGetNumChildren()){
+			printChildren((SimpleNode)node.jjtGetChild(i));
+			i++;
+		}
 	}
 }
