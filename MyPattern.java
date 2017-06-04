2c1448c998bb9453295aaa6408285395d099c0fc
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-06-04 19:28:48 +0100

    New patterns.txt

--- MyPattern.java
+++ MyPattern.java
@@ -1,25 +1,34 @@
-public class MyPattern{
-void P1(String Exhaustive){
-ignore();
-long x = 0;
-x = a0*a0;
-}
-void P2(String Exhaustive){
-if(a0==a1){
-int a;
-ignore();
-a = 3;
-a2();
-}}
-void P3(String Exhaustive){
-if(a0){
-a = 3;
-}for(int i = 0;i<a3;i++){
-ignore();
-a = i+1;
-}}
-void P4(String Exhaustive){
-do{
-a++;
-}while(true)}
- }
+public class MyPattern{
+void P1(String Exhaustive){
+ignore();
+int a0 = 10;
+ignore();
+if(a0==a1){
+a0--;
+a0++;
+}ignore();
+}
+void P2(String Exhaustive){
+if(a0==a1){
+int a2;
+ignore();
+a2 = 3;
+a3();
+}}
+void P3(String Exhaustive){
+if(a0){
+a2 = 3;
+}for(int i = 0;i<a4;i++){
+ignore();
+a2 = i+1;
+}}
+void P4(String Exhaustive){
+do{
+a2++;
+}while(true)}
+void P5(String Exhaustive){
+if(a0==a1){
+a0--;
+a0++;
+}}
+ }
