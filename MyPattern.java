a3d6ab50f28f883d0442369ea17849de14448938
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-06-04 20:04:39 +0100

    Patterns

--- MyPattern.java
+++ MyPattern.java
@@ -2,23 +2,23 @@ public class MyPattern{
 void P1(String Exhaustive){
 ignore();
 int a0 = 10;
-ignore();
 if(a0==a1){
 a0--;
 a0++;
 }ignore();
 }
 void P2(String Exhaustive){
-if(a0==a1){
-int a2;
 ignore();
-a2 = 3;
-a3();
-}}
+int a0 = 10;
+ignore();
+for(int i = 0;i<a0;i++){
+a1(a0);
+}ignore();
+}
 void P3(String Exhaustive){
 if(a0){
 a2 = 3;
-}for(int i = 0;i<a4;i++){
+}for(int i = 0;i<a3;i++){
 ignore();
 a2 = i+1;
 }}
