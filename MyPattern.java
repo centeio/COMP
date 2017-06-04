5d356c19e34a31260b2eae7643aef2a9fbdaf654
centeio <c.centeiojorge@outlook.pt>
2017-06-04 17:32:33 +0100

    SEMICOL error detected and solved

--- MyPattern.java
+++ MyPattern.java
@@ -1,22 +1,24 @@
 public class MyPattern{
-void P1("Exhaustive"){
+void P1(String Exhaustive){
 ignore();
+long x = 0;
 x = a0*a0;
 }
-void P2("Exhaustive"){
+void P2(String Exhaustive){
 if(a0==a1){
+int a;
 ignore();
 a = 3;
 a2();
 }}
-void P3("Exhaustive"){
+void P3(String Exhaustive){
 if(a0){
 a = 3;
 }for(int i = 0;i<a3;i++){
 ignore();
 a = i+1;
 }}
-void P4("Exhaustive"){
+void P4(String Exhaustive){
 do{
 a++;
 }while(true)}
