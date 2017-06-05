71f72728a0bbd74595b3f0ec6c170c7c7594b0eb
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-06-05 18:53:14 +0100

    Test java inside testsuite

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ rm -r bin
 find src/ -name "*.java" > sources.txt
 mkdir bin
 javac -d ./bin -cp ".:spoon2ast.jar:gson-2.8.0.jar:" @sources.txt
-cp Test.java bin/
+cp testsuite/Test.java bin/
 rm sources.txt
