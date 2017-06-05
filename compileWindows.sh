5199c605384ed1c24af05e245f616189ab01dc89
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-06-05 19:50:30 +0100

    Added windows scripts

---
+++ compileWindows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r bin
+find src/ -name "*.java" > sources.txt
+mkdir bin
+javac -d ./bin -cp ".;spoon2ast.jar;gson-2.8.0.jar;" @sources.txt
+cp testsuite/Test.java bin/
+rm sources.txt
