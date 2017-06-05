5b86a857ec5505039ec619bc6a50ead03477ff3d
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-06-05 04:07:36 +0100

    Test Patterns

--- MyPattern.java
+++ MyPattern.java
@@ -1,42 +1,9 @@
-<<<<<<< HEAD
 public class MyPattern{
 void P1(String Exhaustive){
 ignore();
-long x = 0;
-x = a0*a0;
-}
-void P2(String Exhaustive){
-if(a0==a1){
-int a;
+int a0 = a1;
 ignore();
-a = 3;
-a2();
-}}
-void P3(String Exhaustive){
-if(a0){
-a = 3;
-}int b[];
-for(int i = 0;i<a3;i++){
-b[i] = b[3*i];
-ignore();
-a = i+1;
-}}
-void P4(String Exhaustive){
-do{
-a++;
-}while(true)}
-void P1(String Exhaustive){
-if(a0==a1){
-a0--;
-a0++;
-}}
- }
-=======
-public class MyPattern{
-void P1(String Exhaustive){
-ignore();
-int a0 = 10;
-if(a0==a1){
+if(a0==a2){
 a0--;
 a0++;
 }ignore();
@@ -46,24 +13,21 @@ ignore();
 int a0 = 10;
 ignore();
 for(int i = 0;i<a0;i++){
-a1(a0);
+a2(a0);
 }ignore();
 }
 void P3(String Exhaustive){
-if(a0){
-a2 = 3;
-}for(int i = 0;i<a3;i++){
 ignore();
-a2 = i+1;
-}}
-void P4(String Exhaustive){
-do{
+int a2 = a1;
+while(a2<a0){
+data[a2]--;
 a2++;
-}while(true)}
-void P5(String Exhaustive){
-if(a0==a1){
+}ignore();
+}
+void P4(String Exhaustive){
+ignore();
+do{
 a0--;
-a0++;
-}}
+a2++;
+}while(a2<a0)}
  }
->>>>>>> master
